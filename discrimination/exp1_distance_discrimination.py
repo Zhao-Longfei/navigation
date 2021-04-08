c12fefbb783b2d47745463ff5554e28b3379c8fb
Zhao-Longfei <69340023+Zhao-Longfei@users.noreply.github.com>
2021-04-08 15:42:30 +0900

    upload .exe

--- discrimination/exp1_distance_discrimination.py
+++ discrimination/exp1_distance_discrimination.py
@@ -12,46 +12,60 @@ def cv2ImgAddText(img, text, left, top, textColor=(0, 255, 0), textSize=20):
     draw.text((left, top), text, textColor, font=fontStyle)
     return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
-def Drawing_random_Lines(image,num_test):
+def random_distance(image,num_test):
     h, w = image.shape[0],image.shape[1]
-    x1 = random.uniform(0.5 * w - 75, 0.5 * w)
-    y1 = 0.5 * h - np.sqrt(pow(75, 2)-pow(0.5 * w-x1, 2))
+    length = 150
+    x1 = random.uniform(0.5 * w - length / 2, 0.5 * w)
+    y1 = 0.5 * h - np.sqrt(pow(length / 2, 2)-pow(0.5 * w-x1, 2))
     (x2, y2) = (w-x1, h-y1)
 
     o = np.subtract(y2, y1)
     q = np.subtract(x2, x1)
     slope= - q / o
-    d=random.uniform(0,5)
+    '''
+    d=0.1 * random.randint(0,50)
     
     a1=x1 + np.sqrt(abs(pow(d ,2)/(pow(slope,2)-1)))
     b1=y1 + slope * (a1 - x1)
 
     a2=x2 + np.sqrt(abs(pow(d ,2)/(pow(slope,2)-1)))
     b2=y2 + slope * (a2 - x2)
+    '''
+    d = 0.1 * random.randint(0,50)
+    alpha = 0#0.1 * random.randint(0, 60)
+    
+    a1=x1 + (d - length /2 * np.sin(alpha * np.pi / 180)) / np.sqrt(pow(slope ,2) + 1)
+    b1=y1 + slope * (a1 - x1)
+
+    a2=x2 + (d + length /2 * np.sin(alpha * np.pi / 180)) / np.sqrt(pow(slope ,2) + 1)
+    b2=y2 + slope * (a1 - x1)
 
     cv2.line(image,(int(x1), int(y1)), (int(x2), int(y2)), (0, 225, 0), 1, cv2.LINE_AA)
-    image1=image
-    cv2.line(image1,(int(a1), int(b1)), (int(a2), int(b2)), (0, 225, 0), 1, cv2.LINE_AA)
+    image=image
+    cv2.line(image,(int(a1), int(b1)), (int(a2), int(b2)), (0, 225, 0), 1, cv2.LINE_AA)
 
     text1 = str(num_test)+"/500"
     text2 = "差がある → Type'Y'"
     text3 = "分からない、迷った → Type'N'"
     text4 = "戻る → Type'ESC'"
-    image1=cv2ImgAddText(image1,text1 ,20,20, (0,0,0),20)
-    image1=cv2ImgAddText(image1,text2 ,30,730, (0,0,0),15)
-    image1=cv2ImgAddText(image1,text3 ,30,750, (0,0,0),15)
-    image1=cv2ImgAddText(image1,text4 ,30,770, (0,0,0),15)
+    image=cv2ImgAddText(image,text1 ,20,20, (0,0,0),20)
+    image=cv2ImgAddText(image,text2 ,30,730, (0,0,0),15)
+    image=cv2ImgAddText(image,text3 ,30,750, (0,0,0),15)
+    image=cv2ImgAddText(image,text4 ,30,770, (0,0,0),15)
 
-    return image1,d
+    return image,d
 
 data = [['disatance','YES_NO']]
+
 num_test=num_yes=num_no=0
 while True and num_test<500:
 
     image=np.zeros([800,800,3], np.uint8)+255
-    img, d =Drawing_random_Lines(image,num_test)
-    cv2.imshow('test', img)
-    
+    img, d =random_distance(image,num_test)
+    d = round(d , 1)
+    cv2.imshow('Discrimination ability experiment', img)
+
+    print(d)
     key = cv2.waitKey(0)
     if key == ord('y'):
         print("yes")
@@ -72,7 +86,7 @@ while True and num_test<500:
 #print("yes:",num_yes)
 #print("no:",num_no)
 #print(data)
-output = open('data.xls','w',encoding='gbk')
+output = open('data_distance.xls','w',encoding='gbk')
 for i in range(len(data)):
 	for j in range(len(data[i])):
 		output.write(str(data[i][j]))    
--- discrimination/exp2_angle_discrimination.py
+++ discrimination/exp2_angle_discrimination.py
@@ -4,38 +4,88 @@ import numpy as np
 from PIL import Image,ImageFont,ImageDraw
 import os
 
+def cv2ImgAddText(img, text, left, top, textColor=(0, 255, 0), textSize=20):
+    if (isinstance(img, np.ndarray)): 
+        img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(img)
+    fontStyle = ImageFont.truetype("font/simsun.ttc", textSize, encoding="utf-8")
+    draw.text((left, top), text, textColor, font=fontStyle)
+    return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
-def Drawing_random_Lines(image):
+def random_angle(image,num_test):
     h, w = image.shape[0],image.shape[1]
-    x1 = random.randint(0.5 * w - 75, 0.5 * w)
-    y1 = 0.5 * h - np.sqrt(pow(75, 2)-pow(0.5 * w-x1, 2))
+    length = 150
+    x1 = random.uniform(0.5 * w - length / 2, 0.5 * w)
+    y1 = 0.5 * h - np.sqrt(pow(length / 2, 2)-pow(0.5 * w-x1, 2))
     (x2, y2) = (w-x1, h-y1)
 
     o = np.subtract(y2, y1)
     q = np.subtract(x2, x1)
-    slope= - q / o
-    d=random.randint(0,10)
+    slope = - q / o
+
+    #d_min = 6.55
+    d = 10
+    alpha = 0.1 * random.randint(0, 60)
     
-    a1=x1 + np.sqrt(abs(pow(d ,2)/(pow(slope,2)-1)))
+    a1=x1 + (d - length /2 * np.sin(alpha * np.pi / 180)) / np.sqrt(pow(slope ,2) + 1)
     b1=y1 + slope * (a1 - x1)
 
-    a2=x2 + np.sqrt(abs(pow(d ,2)/(pow(slope,2)-1)))
-    b2=y2 + slope * (a2 - x2)
+    a2=x2 + (d + length /2 * np.sin(alpha * np.pi / 180)) / np.sqrt(pow(slope ,2) + 1)
+    b2=y2 + slope * (a1 - x1)
+
 
     cv2.line(image,(int(x1), int(y1)), (int(x2), int(y2)), (0, 225, 0), 1, cv2.LINE_AA)
-    image1=image
-    cv2.line(image1,(int(a1), int(b1)), (int(a2), int(b2)), (0, 225, 0), 1, cv2.LINE_AA)
-    return image1
+    image=image
+    cv2.line(image,(int(a1), int(b1)), (int(a2), int(b2)), (0, 225, 0), 1, cv2.LINE_AA)
 
-relative_path = "plan/002.jpg"
-script_path = os.path.dirname(__file__)
-#path = 'plan/002.jpg'
-image=cv2.imread(os.path.join(script_path, relative_path),1)
-image=np.zeros([800,800,3], np.uint8)+255
+    text1 = str(num_test)+"/500"
+    text2 = "差がある → Type'Y'"
+    text3 = "分からない、迷った → Type'N'"
+    text4 = "戻る → Type'ESC'"
+    image=cv2ImgAddText(image,text1 ,20,20, (0,0,0),20)
+    image=cv2ImgAddText(image,text2 ,30,730, (0,0,0),15)
+    image=cv2ImgAddText(image,text3 ,30,750, (0,0,0),15)
+    image=cv2ImgAddText(image,text4 ,30,770, (0,0,0),15)
 
-image1=Drawing_random_Lines(image)
+    return image,alpha
 
 
+data = [['angle','YES_NO']]
 
-cv2.imshow("image", image1)
-cv2.waitKey(0)
+num_test=num_yes=num_no=0
+while True and num_test<500:
+
+    image=np.zeros([800,800,3], np.uint8)+255
+ 
+    img, alpha =random_angle(image,num_test)
+    alpha = round(alpha, 1)
+
+    cv2.imshow('Discrimination ability experiment', img)
+
+    key = cv2.waitKey(0)
+    if key == ord('y'):
+        print("yes")
+        data.append([alpha,1])
+        num_test += 1
+        num_yes += 1
+
+    if key == ord('n'):
+        print("no")
+        data.append([alpha,0])
+        num_test += 1
+        num_no += 1
+
+    if key == 27:
+        print("End of program")
+        break
+
+#print("yes:",num_yes)
+#print("no:",num_no)
+#print(data)
+output = open('data_angle.xls','w',encoding='gbk')
+for i in range(len(data)):
+	for j in range(len(data[i])):
+		output.write(str(data[i][j]))    
+		output.write('\t')   
+	output.write('\n')      
+output.close()
